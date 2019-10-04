b4d8ff5ee6f12fb3cdc9058223b5a503d6fa4cbd
Steven J. Selcuk <stevenjselcuk@gmail.com>
2019-10-04 20:36:07 +0300

    Production webpack config

---
+++ webpack.config.production.babel.js
@@ -0,0 +1,27 @@
+module.exports = {
+	entry: ["./src"],
+	target: "node",
+	mode: 'production',
+	watch: false,
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ["eslint-loader"]
+			}
+		]
+	},
+	output: {
+		path: `${__dirname}/dist`,
+		publicPath: "/",
+		filename: "server.js"
+	},
+	plugins: [
+	]
+};

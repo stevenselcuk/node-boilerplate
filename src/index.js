9b29ea1a315644cd71185b5b72e15e0c69a4e804
Steven J. Selcuk <stevenjselcuk@gmail.com>
2019-10-05 15:30:51 +0300

    m

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ log.info(`Boilerplate version ${conf.get('VERSION')}`)
 log.info(`A variable from your config.json: ${conf.get('YOUR_SECRET_STUFF')}`)
 show.tabby('Hello a cat is here...')
 
+log.info(`Some stufff${conf.get('VERSION')}`)
+
+console.log('Hi')
 process.on('SIGINT', () => {
   process.exit(1)
 })

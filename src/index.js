62418c48b4bf119865a4db669c51ad813b07fc48
Steven J. Selcuk <stevenjselcuk@gmail.com>
2019-10-05 14:41:45 +0300

    🐛 FIX: package.json typos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import conf from './utils/config'
 import { log, show } from './utils/logger'
 
-log.info(`Boilerplate version ${  conf.get('VERSION')}`)
+log.info(`Boilerplate version ${conf.get('VERSION')}`)
 log.info(`A variable from your config.json: ${conf.get('YOUR_SECRET_STUFF')}`)
 show.tabby('Hello a cat is here...')
 
-log.info('Some stufff' + conf.get('VERSION'))
-
-console.log('Hi')
 process.on('SIGINT', () => {
   process.exit(1)
 })
 
-process.on('exit', (code) => {
-  console.log(`About to exit with code: ${code}`);
-});
+process.on('exit', code => {
+  console.log(`About to exit with code: ${code}`)
+})
 
 process.on('uncaughtException', e => {
   console.log(e)
